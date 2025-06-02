4404ca9890d91aa89496f61ebf53160147338861
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2025-06-02 19:07:24 +0300

    sensor_reader: Remove comment and unused import

--- src/sensor_reader.py
+++ src/sensor_reader.py
@@ -1,5 +1,4 @@
 from asyncio import sleep
-from aiosqlite import connect
 from datetime import datetime
 from adafruit_dht import DHTBase
 from db.db_thum import ThumDatabase
@@ -44,15 +43,10 @@ async def start_sensor_reader():
 	while True:
 		data = poll_sensor_data()
 
-		# Incase sensor reading fails, add log of the error that occurred to the database
-		# the error that occurred to the database and jump back to the start of the loop;
-		# not waiting the config.get('sensor.interval') ms for new reading.
-		#
-		# Same goes if temperature or humidity is None; just try again, since we want
-		# both of those readings to be inserted in to the database.
 		if not data['success']:
-			print('OOPS, error occurred. Attempting again in 2 seconds...')
+			print('Error occurred. Attempting again in 2 seconds ...')
 			await db.log.insert_log_entry_async(data)
+			await sleep(2)
 			continue
 
 		await db.sensor.insert_sensor_entry_async(data)
