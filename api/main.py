d2d8f95cb9e5a5c82880a7329d8225c8334c5a8d
Joel <11576776+JokkeeZ@users.noreply.github.com>
2025-08-06 11:46:22 +0300

    feat: add CORSMiddleware to FastAPI to allow localhost origin

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from api.db.db_sensor import DatabaseSensor
 from api.sensor import sensor_poll
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
@@ -10,6 +11,15 @@ async def lifespan(_app: FastAPI):
   yield
 
 app = FastAPI(redoc_url=None, docs_url=None, openapi_url=None, lifespan=lifespan)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
 db = DatabaseSensor('test_db.db')
 
 def error_template(e: Exception):
