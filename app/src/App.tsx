c569cba62da22bccb35f273304f20223f251d761
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2025-12-02 13:59:20 +0200

    fix: footer position and remove old css defs

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,40 +14,44 @@ export default function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <header>
-          <NavigationBar />
-        </header>
+        <div className="d-flex flex-column min-vh-100">
+          <header>
+            <NavigationBar />
+          </header>
 
-        <main>
-          <NotificationContainer
-            settings={{
-              dockPosition: "top-right",
-              duration: 3000,
-              slideDirection: "right",
-            }}
-          >
-            <div className="container">
-              <Routes>
-                <Route path="/" element={<HomeView />} />
-                <Route path="/daily" element={<DailyView />} />
-                <Route path="/weekly" element={<WeeklyView />} />
-                <Route path="/monthly" element={<MonthlyView />} />
-                <Route path="/range" element={<RangeView />} />
-                <Route path="/statistics" element={<StatsView />} />
-                <Route path="/logs" element={<LogView />} />
-              </Routes>
+          <main className="flex-grow-1">
+            <NotificationContainer
+              settings={{
+                dockPosition: "top-right",
+                duration: 3000,
+                slideDirection: "right",
+              }}
+            >
+              <div className="container">
+                <Routes>
+                  <Route path="/" element={<HomeView />} />
+                  <Route path="/daily" element={<DailyView />} />
+                  <Route path="/weekly" element={<WeeklyView />} />
+                  <Route path="/monthly" element={<MonthlyView />} />
+                  <Route path="/range" element={<RangeView />} />
+                  <Route path="/statistics" element={<StatsView />} />
+                  <Route path="/logs" element={<LogView />} />
+                </Routes>
+              </div>
+            </NotificationContainer>
+          </main>
+
+          <footer className="py-4">
+            <div className="container d-flex flex-column flex-md-row justify-content-center align-items-center">
+              <div className="mb-1 mb-md-0">
+                <span>Open-source on </span>
+                <a className="text-info" href="https://github.com/jokkeez/thum">
+                  <span>Github</span>
+                </a>
+              </div>
             </div>
-          </NotificationContainer>
-        </main>
-
-        <footer className="text-center text-lg-start footer">
-          <div className="text-center p-3">
-            <span>Open-source on </span>
-            <a className="text-info" href="https://github.com/jokkeez/thum">
-              <span>Github</span>
-            </a>
-          </div>
-        </footer>
+          </footer>
+        </div>
       </BrowserRouter>
     </ThemeProvider>
   );
