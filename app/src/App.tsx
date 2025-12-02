7302370f87e0a15dd0e61ab6e7847313b2131f8a
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2025-12-02 13:47:42 +0200

    style: formatting

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -13,67 +13,42 @@ import { BrowserRouter, Route, Routes } from "react-router";
 export default function App() {
   return (
     <ThemeProvider>
-        <BrowserRouter>
-      <header>
-        <NavigationBar/>
-      </header>
+      <BrowserRouter>
+        <header>
+          <NavigationBar />
+        </header>
 
-      <main>
-        <NotificationContainer
-          settings={{
-            dockPosition: "top-right",
-            duration: 3000,
-            slideDirection: "right",
-          }}
-        >
+        <main>
+          <NotificationContainer
+            settings={{
+              dockPosition: "top-right",
+              duration: 3000,
+              slideDirection: "right",
+            }}
+          >
             <div className="container">
               <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <HomeView/>
-                  }
-                />
-                <Route
-                  path="/daily"
-                  element={
-                    <DailyView/>
-                  }
-                />
-                <Route
-                  path="/weekly"
-                  element={
-                    <WeeklyView/>
-                  }
-                />
-                <Route
-                  path="/monthly"
-                  element={
-                    <MonthlyView/>
-                  }
-                />
-                <Route
-                  path="/range"
-                  element={
-                    <RangeView/>
-                  }
-                />
+                <Route path="/" element={<HomeView />} />
+                <Route path="/daily" element={<DailyView />} />
+                <Route path="/weekly" element={<WeeklyView />} />
+                <Route path="/monthly" element={<MonthlyView />} />
+                <Route path="/range" element={<RangeView />} />
                 <Route path="/statistics" element={<StatsView />} />
                 <Route path="/logs" element={<LogView />} />
               </Routes>
             </div>
-        </NotificationContainer>
-      </main>
+          </NotificationContainer>
+        </main>
 
-      <footer className="text-center text-lg-start footer">
-        <div className="text-center p-3">
-          <span>Open-source on </span>
-          <a className="text-info" href="https://github.com/jokkeez/thum">
-            <span>Github</span>
-          </a>
-        </div>
-      </footer>
-          </BrowserRouter>
+        <footer className="text-center text-lg-start footer">
+          <div className="text-center p-3">
+            <span>Open-source on </span>
+            <a className="text-info" href="https://github.com/jokkeez/thum">
+              <span>Github</span>
+            </a>
+          </div>
+        </footer>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
