5782db37509d747fab564b2f053e9e243a1617b6
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2025-12-26 00:07:24 +0200

    feat(utils): remove unused min-max function

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -1,5 +1,3 @@
-import type { IMinMaxValues } from "../types/IMinMaxValuesLoaded";
-
 /**
  * Checks if the browser is Chromium-based.
  * @returns {boolean} true if browser is Chromium-based
@@ -7,12 +5,4 @@ import type { IMinMaxValues } from "../types/IMinMaxValuesLoaded";
 export function isChromiumBased(): boolean {
   const ua = navigator.userAgent;
   return /Chrome|Chromium/i.test(ua);
-}
-
-export async function fetchMinMaxValues(
-  url: string
-): Promise<IMinMaxValues> {
-  const resp = await fetch(url);
-  const json: IMinMaxValues = await resp.json();
-  return { first: json.first, last: json.last, loaded: true };
 }
