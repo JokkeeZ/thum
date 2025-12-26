e44e86548e1597b0fd0d86118c6d0677436809a3
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2025-12-26 18:02:42 +0200

    chore: rename style_new.css to thum.css and move to src

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "bootswatch/dist/minty/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import "./styles/style_new.css";
+import "./thum.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(<App />);
