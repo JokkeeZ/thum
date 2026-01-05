cf788fb40e504c49aeaa2c0638dcd17439fd486a
Joel <11576776+JokkeeZ@users.noreply.github.com>
2026-01-05 11:31:33 +0200

    fix(api)!: return status_code 400 when request fails

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from contextlib import asynccontextmanager
 from fastapi.responses import FileResponse
 from api.db.database import Database
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from api.models.app_config import AppConfig
 from api.models.status_response import StatusResponse
@@ -42,46 +42,47 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
-def error_template(e: Exception) -> StatusResponse:
+def fail_with_http_400(response: Response, e: Exception) -> StatusResponse:
+  response.status_code = status.HTTP_400_BAD_REQUEST
   return StatusResponse(success=False, message=str(e))
 
 @app.get('/api/sensor/all')
-async def all() -> list[SensorEntry] | StatusResponse:
+async def all(response: Response) -> list[SensorEntry] | StatusResponse:
   try:
     return await db.all_async()
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/sensor/monthly/{year}/{month}')
-async def monthly(year: int, month: int) -> list[SensorEntry] | StatusResponse:
+async def monthly(year: int, month: int, response: Response) -> list[SensorEntry] | StatusResponse:
   try:
     return await db.by_month_async(year, month)
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/sensor/weekly/{week}')
-async def weekly(week: str):
+async def weekly(week: str, response: Response):
   try:
     return await db.by_week_async(week)
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/sensor/daily/{day}/{month}/{year}')
-async def daily(day: int, month: int, year: int) -> list[SensorEntry] | StatusResponse:
+async def daily(day: int, month: int, year: int, response: Response) -> list[SensorEntry] | StatusResponse:
   try:
     return await db.by_date_async(day, month, year)
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/sensor/range/{start_date}/{end_date}')
-async def range(start_date: str, end_date: str) -> list[SensorEntry] | StatusResponse:
+async def range(start_date: str, end_date: str, response: Response) -> list[SensorEntry] | StatusResponse:
   try:
     return await db.by_range_async(start_date, end_date)
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/sensor/current')
-async def current() -> LiveSensor | StatusResponse:
+async def current(response: Response) -> LiveSensor | StatusResponse:
   if not db.config.use_sensor:
     return StatusResponse(success=False, message='Sensor is not available.')
 
@@ -95,42 +96,42 @@ async def current() -> LiveSensor | StatusResponse:
 
     return LiveSensor(success=True, temperature=temp, humidity=humi)
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/daterange')
-async def get_daterange():
+async def get_daterange(response: Response):
   try:
     return await db.daterange_async()
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/logs')
-async def get_logs_all() -> list[LogEntry] | StatusResponse:
+async def get_logs_all(response: Response) -> list[LogEntry] | StatusResponse:
   try:
     return await db.all_logs_async()
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.delete('/api/logs/{timestamp}')
-async def remove_log_by_timestamp(timestamp: str) -> LogDeleteResult | StatusResponse:
+async def remove_log_by_timestamp(timestamp: str, response: Response) -> LogDeleteResult | StatusResponse:
   try:
     return await db.delete_log_by_ts_async(timestamp)
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.delete('/api/logs')
-async def remove_all_logs() -> LogDeleteResult | StatusResponse:
+async def remove_all_logs(response: Response) -> LogDeleteResult | StatusResponse:
   try:
     return await db.delete_all_logs_async()
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/statistics')
-async def get_statistics() -> StatisticEntry | StatusResponse:
+async def get_statistics(response: Response) -> StatisticEntry | StatusResponse:
   try:
     return await db.statistics_async()
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/')
 async def get_all_urls_from_request(request: Request) -> list[str]:
@@ -141,7 +142,7 @@ async def get_app_config() -> AppConfig:
   return db.config
 
 @app.put('/api/config')
-async def update_config(cfg: AppConfig) -> StatusResponse:
+async def update_config(cfg: AppConfig, response: Response) -> StatusResponse:
   try:
     await db.update_config_async(cfg)
     await db.configure_async()
@@ -155,7 +156,7 @@ async def update_config(cfg: AppConfig) -> StatusResponse:
 
     return StatusResponse(success=True, message='Configuration updated successfully!')
   except Exception as e:
-    return error_template(e)
+    return fail_with_http_400(response, e)
 
 @app.get('/api/dump')
 async def get_database_dump() -> FileResponse:
