66dcacfa5ece8eaff3580ba424269809bfdde5c9
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2026-01-10 16:08:30 +0200

    feat: add mt-4 to CenteredSpinnyLoader

--- app/src/components/CenteredSpinnyLoader.tsx
+++ app/src/components/CenteredSpinnyLoader.tsx
@@ -2,7 +2,7 @@ import SpinnyLoader from "./SpinnyLoader";
 
 export default function CenteredSpinnyLoader() {
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center mt-4">
       <SpinnyLoader width={50} height={50} />
     </div>
   );
