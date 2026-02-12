02a968bc60c638aa670da4404e62ff5f6f485fb0
Jokke <11576776+JokkeeZ@users.noreply.github.com>
2026-02-12 13:51:26 +0200

    feat: StatisticsItem component for statistics

--- app/src/components/StatisticsList.tsx
+++ app/src/components/StatisticsList.tsx
@@ -1,50 +1,59 @@
 import type { IStatisticsResponse } from "../types/IStatisticsResponse";
 
+function StatisticsItem(props: {
+  label: string;
+  value: string | number;
+  unit: "" | "°C" | "%";
+}) {
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {props.label}
+      <span className="badge bg-primary">
+        {props.value}
+        {props.unit}
+      </span>
+    </li>
+  );
+}
+
 export default function StatisticsList(props: { stats: IStatisticsResponse }) {
   return (
     <ul className="list-group list-group-flush">
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Total Entries
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.total_entries}
-        </span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Average Temperature
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.avg_temperature.toFixed(2)}°C
-        </span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Average Humidity
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.avg_humidity.toFixed(2)}%
-        </span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Maximum Temperature — {props.stats?.max_temperature.date}
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.max_temperature.value}°C
-        </span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Minimum Temperature — {props.stats?.min_temperature.date}
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.min_temperature.value}°C
-        </span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Maximum Humidity — {props.stats?.max_humidity.date}
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.max_humidity.value}%
-        </span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        Minimum Humidity — {props.stats?.min_humidity.date}
-        <span className="badge bg-primary rounded-pill">
-          {props.stats?.min_humidity.value}%
-        </span>
-      </li>
+      <StatisticsItem
+        label="Total Entries"
+        value={props.stats?.total_entries}
+        unit=""
+      />
+      <StatisticsItem
+        label="Average Temperature"
+        value={props.stats?.avg_temperature.toFixed(2)}
+        unit="°C"
+      />
+      <StatisticsItem
+        label="Average Humidity"
+        value={props.stats?.avg_humidity.toFixed(2)}
+        unit="%"
+      />
+      <StatisticsItem
+        label={`Max. Temperature — ${props.stats?.max_temperature.date}`}
+        value={props.stats?.max_temperature.value}
+        unit="°C"
+      />
+      <StatisticsItem
+        label={`Min. Temperature — ${props.stats?.min_temperature.date}`}
+        value={props.stats?.min_temperature.value}
+        unit="°C"
+      />
+      <StatisticsItem
+        label={`Max. Humidity — ${props.stats?.max_humidity.date}`}
+        value={props.stats?.max_humidity.value}
+        unit="%"
+      />
+      <StatisticsItem
+        label={`Min. Humidity — ${props.stats?.min_humidity.date}`}
+        value={props.stats?.min_humidity.value}
+        unit="%"
+      />
     </ul>
   );
 }
